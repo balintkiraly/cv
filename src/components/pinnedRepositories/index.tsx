0284695b66f290dcb35ef9ef2e32a674295260dc
balintkiraly <balint.kiraly@protonmail.com>
2020-04-12 11:40:54 +0200

    Fix repositories spacing

--- src/components/pinnedRepositories/index.tsx
+++ src/components/pinnedRepositories/index.tsx
@@ -33,7 +33,7 @@ export const PinnedRepositories = (): JSX.Element => {
     }
   `);
   return (
-    <SimpleGrid columns={2} spacing={10} my={3}>
+    <SimpleGrid columns={2} spacing={4} my={3}>
       {data.repositories.edges.map(({ node }: any) => (
         <Repository
           title={node.childMarkdownRemark.frontmatter.title}
