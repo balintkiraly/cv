992adfb83cc412b040ea9686d2f9d22465a6e630
balintkiraly <balint.kiraly@protonmail.com>
2021-05-21 19:57:30 +0200

    Improve dates

--- src/components/dataRange.tsx
+++ src/components/dataRange.tsx
@@ -1,4 +1,4 @@
-import { Text } from '@chakra-ui/react';
+import { Text, useColorModeValue } from '@chakra-ui/react';
 import React from 'react';
 
 interface DataRangeProps {
@@ -8,9 +8,13 @@ interface DataRangeProps {
 export const DataRange = ({
   startDate,
   endDate,
-}: DataRangeProps): JSX.Element => (
-  <Text fontSize="xs" pt={1} color="gray.600">
-    {startDate !== 'Invalid date' && startDate} -{' '}
-    {endDate !== 'Invalid date' && endDate}
-  </Text>
-);
+}: DataRangeProps): JSX.Element => {
+  const textColor = useColorModeValue('gray.600', 'gray.500');
+
+  return (
+    <Text fontSize="xs" pt={1} color={textColor}>
+      {startDate !== 'Invalid date' && startDate} -{' '}
+      {(endDate !== 'Invalid date' && endDate) || 'Present'}
+    </Text>
+  );
+};
